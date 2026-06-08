b3606565fdd7121e9d0b7aba84680bfcc9e58a3c
Priyanshu Kumar <upiyush005@gmail.com>
2026-06-08 15:10:41 +0530

    image fix

--- src/components/account-dropdown.tsx
+++ src/components/account-dropdown.tsx
@@ -93,7 +93,7 @@ export default function AccountDropdown({
               <UserAvatar
                 name={data?.admin.fullName || user?.fullName || '-'}
                 image={
-                  data?.admin.fullName || user?.profileImage || ''
+                  data?.admin.profileImage || user?.profileImage || ''
                 }
                 size="sm"
               />
@@ -109,8 +109,8 @@ export default function AccountDropdown({
             </div>
             <div className="hidden sm:flex items-center gap-3">
               <UserAvatar
-                name={user?.fullName ?? ''}
-                image={user?.profileImage}
+                name={data?.admin.fullName || user?.fullName || ''}
+                image={data?.admin.profileImage || user?.profileImage}
                 size="sm"
               />
               <span className="text-sm font-semibold">
